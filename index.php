e846bc9157072ba6029199e93cc64e55d79a1b81
sw1ckham <sphwick@gmail.com>
2020-08-04 17:14:30 +0100

    Mabels front-page

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html lang="en">
+<head>
+<title>Mabels-Over 50s assisted retirement living,bygone times nostalgia,uk over 50 over 
+50's uk seniors</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="expires" content="-1" >
+<meta http-equiv= "pragma" content="no-cache" >
+<meta name="author" content="Jon Wickham,www.mabels.org.uk" >
+<meta name="robots" content="all" >
+<meta name="MSSmartTagsPreventParsing" content="true" >
+<meta name="description" content="For the over 50s, providing daily retirement support & services,assisted disability living & mobility health retirement & finance tips nostalgic goods & gifts leisure & hobbies for the over 50s & senior citizens" >
+<meta name="keywords" content="Mabels. mabels.org,assisted living,mobility,disability aids,senior citizens,over 50,over 50's,over 50s,50 plus,
+ 50,50s,50s over, maintaining bygone times,retirement,bygone times,mabel,mables,retirement,senior,seniors,
+ activity elderly,nostalgic goods,Nostalgia,over fifty,
+ uk ,senior uk,disability aids,disabled equipment,
+ traditional goods" >
+<link rel="Home" href="#" >
+<link href="favicon.ico" type="image/ico" rel="icon">
+<!--printing stylesheet-->
+<link media="print" href="css/print.css" type="text/css" rel="stylesheet">
+<!--basic layout stylesheet-->
+<style type="text/css" media="screen">@import url(css/layout.css);</style>
+<!--text styling for page-->
+<!--<style type="text/css" media="screen">@import url(css/textformat.css);</style>-->
+<!--menu basic layout stylesheet-->
+<style type="text/css" media="screen">@import url(css/menulayout.css);</style>
+<!--menu presentation stylesheet-->
+<style type="text/css" media="screen">@import url(css/menu.css);</style>
+<!--default theme stylesheet-->
+<style type="text/css" media="screen">@import url(css/default.css);</style>
+<!--hides tag from non-javascript browser-->
+<script type="text/javascript">
+<!--
+document.write("<LINK id='hoverJS' media='all' href='css/menuhover.css' type=text/css rel='stylesheet' >");
+-->
+</script>
+
+<script src="js/dhtml.js" type="text/javascript"></script>
+
+<script src="js/menu.js" type="text/javascript"></script>
+
+<script src="js/config.js" type="text/javascript"></script>
+
+</script>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Begin
+// Script by Tr�nn: http://come.to/tronds
+// Courtesy of SimplytheBest.net - http://simplythebest.net/scripts/
+
+var initialsubj="Hi, take a look at this web-site"
+var initialmsg="Hi:\n\n You may want to check out this site: "+window.location
+var good;
+function checkEmailAddress(field) {
+
+var goodEmail = field.value.match(/\b(^(\S+@).+((\.com)|(\.net)|(\.edu)|(\.mil)|(\.gov)|(\.org)|(\.info)|(\.sex)|(\.biz)|(\.aero)|(\.coop)|(\.museum)|(\.name)|(\.pro)|(\..{2,2}))$)\b/gi);
+if (goodEmail) {
+good = true;
+}
+else {
+alert('Please enter a valid address.');
+field.focus();
+field.select();
+good = false;
+   }
+}
+u = window.location;
+function mailThisUrl() {
+good = false
+checkEmailAddress(document.eMailer.email);
+if (good) {
+
+//window.location = "mailto:"+document.eMailer.email.value+"?subject="+initialsubj+"&body="+document.title+" "+u;
+window.location = "mailto:"+document.eMailer.email.value+"?subject="+initialsubj+"&body="+initialmsg
+   }
+}
+//  End -->
+</script>
+
+</head>
+
+<body onload="init()">
+<!--header-->
+  <h1 align="center"><a href="https://www.mabels.org.uk/forum/">Forums</a><h1>
+  <h1 align="center"><a href="https://www.mabels.org.uk/retirement-jokes.htm">Retirement Jokes</a><h1>
+  <h1 align="center"><a href="https://www.mabels.org.uk/sitemap-articles.php">Articles</a><h1>
+<!--/header-->
+<!--main-->
+<div id="main"> <a name="nav"></a> 
+  <? include("left-nav.inc.php"); ?>
+  <!--/navbar-->
+  <!--content-->
+  <a name="content"></a> 
+  <div id="content"> 
+    <h3 align="center"><br>
+      <font face="Geneva, Arial, Helvetica, san-serif" color="#990033"><br>
+      </font></h3>
+    <h4 align="center"><font color="#660000"><b><b><b><b>The essential resource 
+      for all mature people -<br>
+      to help you make the most out of your life!</b></b></b></b></font></h4>
+    <h3 align="center"><span class="style5"><font face="Arial, Helvetica, sans-serif"><i><font color="#003366"><span class="style5"><font face="Arial, Helvetica, sans-serif"><b><font color="#990033"><font face="Arial, Helvetica, sans-serif"><b><font color="#990033"><font color="#006666"><b><font face="Arial, Helvetica, sans-serif" size="2"><font size="3"><span class="style5"><i><font face="Arial, Helvetica, sans-serif"><b><font color="#990033"><span class="style5"><font face="Arial, Helvetica, sans-serif"><i><font color="#003366"><span class="style5"><font face="Arial, Helvetica, sans-serif"><b><font color="#990033"><font face="Arial, Helvetica, sans-serif"><b><font color="#990033"><font color="#006666"><b><font face="Arial, Helvetica, sans-serif" size="2"><font size="3"><span class="style5"><i><font face="Arial, Helvetica, sans-serif"><b><font color="#006666"> 
+      <br>
+      <img src="images/5_pics_across.jpg" width="350" height="64"> </font></b></font></i></span></font></font></b></font></font></b></font></font></b></font></span></font></i></font></span></font></b></font></i></span></font></font></b></font></font></b></font></font></b></font></span></font></i></font></span><br>
+      <br>
+    </h3>
+    <p align="center"><b>How To Get Started:</b><br>
+      Fill in your details and we'll give you access to <b> Latest Retirement, 
+      Health &amp; Finance News &amp; Advice</b>, the <b>FREE Mabels Newsletter</b>, 
+      <b>Special Offers, Jokes, </b>and much, much more to benefit <b>&quot;Your 
+      Quality of Life&quot;</b>: <br>
+      <!-- GetResponse subscription form | start -->
+    <form action="http://www.getresponse.com/cgi-bin/add.cgi" method="post" id="GRSubscribeForm" accept-charset="UTF-8">
+      <div align="center"><fieldset> 
+        <table>
+          <tr> 
+            <td> <label for="GRCategory2">Your Name</label>: </td>
+            <td> 
+              <input type="text" name="category2" size="20" id="GRCategory2" />
+            </td>
+          </tr>
+          <tr> 
+            <td><label for="GRCategory3">Your E-Mail</label>:</td>
+            <td> 
+              <input type="text" name="category3" size="20" id="GRCategory3" />
+            </td>
+          </tr>
+        </table>
+        <font face="Arial, Helvetica, sans-serif">Your privacy is assured and 
+        you can unsubscribe at any time. </font><br>
+        <input type="submit" value="Send me the Info!" />
+        </fieldset></div>
+      <fieldset> 
+      <h6 align="center"> 
+        <input type="hidden" name="category1" value="mabels" />
+        <input type="hidden" name="confirmation" value="http://www.mabels.org.uk/thankyou.htm"/>
+        <input type="hidden" name="ref" value="000" />
+      </h6>
+      <h6 align="center">&nbsp;</h6>
+      <h6 align="center"><br>
+      </h6>
+      <h6><b><font color="#006666"> </font></b></h6>
+      </fieldset> 
+    </form>
+    <div align="center"> 
+      <style>
+<!--
+/* form box */
+#GRSubscribeForm fieldset {
+width: 260px; 
+border: 0;
+}
+		
+/* comment about GetResponse */
+#GRSubscribeForm p {
+font-size: x-small;
+}
+		
+/* table used to position form elements */
+#GRSubscribeForm table {
+border: 0;
+}
+		
+-->
+</style>
+      <!-- GetResponse subscription form | end -->
+      <br>
+    </div>
+    <p>&nbsp;</p>
+    <h3>&nbsp;</h3>
+    <p>How to thrive past 55 - <a href="http://www.amazon.co.uk/exec/obidos/redirect?link_code=ur2&camp=1634&tag=mabels-21&creative=6738&path=ASIN/0905852761/qid=1124179536" target="_blank">CLICK 
+      HERE - Concise and scientifically proven advice about how to age well</a><br>
+      <br>
+    </p>
+    <div align="center"> </div>
+    <p align="center">&quot; About time ! I am very pleased to have found a web 
+      site that caters for both specialist and general elderly needs as well as 
+      offering the product choices in the same place.&quot; Carolyn Godfrey<br>
+    </p>
+    <p>&nbsp; </p>
+    <p><br>
+    </p>
+  </div>
+  <div class="rightbar" id="textbar-right"> 
+    <h4>Read Articles</h4>
+    <p> <a href="http://www.mabels.org.uk/besttoknow.htm">Retirement, Health. 
+      Ageing, Finance, Advice etc..</a></p>
+    <h4>Jo's Jottings</h4>
+    <p><a href="http://www.mabels.org.uk/jos-jottings.htm">Latest features, tips 
+      &amp; best articles</a>. <span class="style5"><font color="#006666"><font color="#FF6666" face="Geneva, Arial, Helvetica, san-serif"><font color="#CC0000"><b><i><b><font face="Arial, Helvetica, sans-serif"><a href="http://www.mabels.org.uk/jos-jottings.htm"><img src="images/mabel1.jpg" width="84" height="136" border="0"></a></font></b></i></b></font></font></font></span></p>
+    <h4>Useful Resources</h4>
+    <p><a href="http://www.mabels.org.uk/links.htm">Links to other sites<br>
+      of interest</a>.</p>
+    <h4>Web-site promotion</h4>
+    <p><a href="http://www.mabels.org.uk/mabels-promotion.htm">Add free link,<br>
+      Advertising,<br>
+      Newsletter,<br>
+      Press release & Articles</a>.</p>
+    <h4> Newsletter</h4>
+    <br>
+    <table width="125" border="1" cellspacing="2" cellpadding="10" align="center" bordercolor="#990033" height="77">
+      <tr> 
+        <td bgcolor="#FFFFFF" valign="top" height="72"> 
+          <div align="center"><font color="#003333" face="Arial, Helvetica, sans-serif">Subscribe 
+            to <b>FREE Newsletter</b><br>
+            Find out why?<br>
+            <a href="http://www.mabels.org.uk/newsletter-page.htm">JUST CLICK 
+            HERE</a></font></div>
+        </td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p></p>
+  </div>
+  <div style="clear:both"></div>
+  <!--rightbar-->
+  <!--navbar-->
+  <!--footer-->
+  <? include("footer.inc.php"); ?>
+  <!--/footer-->
+</div>
+<!--/main-->
+<!-- Start of StatCounter Code -->
+<script type="text/javascript" language="javascript">
+var sc_project=386280; 
+var sc_partition=1; 
+</script>
+<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script>
+    <noscript><a href="http://www.statcounter.com/" target="_blank"><img  src="http://c2.statcounter.com/counter.php?sc_project=386280&amp;java=0" alt="web counter" border="0"></a> 
+    </noscript> 
+    <!-- End of StatCounter Code -->
+</body>
+</html>
